772f5b68318e04dbb196b2393285cf36441896d8
Hildegard <hildegard.wiggenhorn@foundersworkshop.com>
2023-03-05 17:13:42 -0500

    example

--- js/basic.js
+++ js/basic.js
@@ -13,7 +13,7 @@ window.onload = () => {
                 y: 20,
                 z: 20
             });
-            entity.setAttribute('material', { color: 'green' } );
+            entity.setAttribute('material', { color: 'red' } );
             entity.setAttribute('gps-new-entity-place', {
                 latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
